31b3869e19b01680f3e60935290a338aece29416
DarrenYourBoy <53564212+DarrenYourBoy@users.noreply.github.com>
2019-10-24 15:49:03 +0800

    写游记功能完成

---
+++ pages/post/create.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <postForm :editDraftData="util" @clearUtil="clearUtil" />
+    <drafts @editDraft="editDraft" />
+  </div>
+</template>
+
+<script>
+import postForm from "@/components/post/postForm";
+import drafts from "@/components/post/drafts";
+export default {
+  components: {
+    postForm,
+    drafts
+  },
+  data() {
+    return {
+      util: {}
+    };
+  },
+  methods: {
+    editDraft(value){
+      this.util = value
+    },
+    clearUtil(){
+      this.util = ''
+    }
+  }
+};
+</script>
+
+<style scoped lang="less">
+.container {
+  width: 1000px;
+  padding-top: 20px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
